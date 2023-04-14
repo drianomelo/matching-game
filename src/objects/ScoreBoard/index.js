d583b8acd3609feeb4390c7fedcf888d2150bd61
Adriano <adrianosenny@gmail.com>
2023-04-14 13:43:52 -0300

    :construction: Create ScoreGame object

---
+++ src/objects/ScoreBoard/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+import PlayerName from "../../components/PlayerName";
+
+const ScoreBoard = () => {
+
+  return `
+    <header class="score-board">
+        ${PlayerName("Player1")}
+        ${PlayerName("Player2")}
+    </header>
+  `;
+};
+
+export default ScoreBoard;
