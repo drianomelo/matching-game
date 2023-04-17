73b8a4e0ae2d449b62b52b1355d09693ecfe06ae
Adriano <adrianosenny@gmail.com>
2023-04-17 12:31:13 -0300

    :sparkles: Now is possible change icon and add display flex

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,9 +1,9 @@
 import "./style.css";
 
-const CardGame = () => {
+const CardGame = (icon = 'bola', alt = "bola de basquete") => {
   return `
         <article class="card-game">
-            
+            <img src = "../../../src/images/${icon}.png" alt="${alt}">
         </article>
     `;
 };
