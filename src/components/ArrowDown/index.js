5b88a13f1ced5b8424c3198b0425eddb50016acb
Adriano <adrianosenny@gmail.com>
2023-04-19 12:42:33 -0300

    :sparkles: Create ArrowDown component with CSS

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const ArrowDown = () => {
+  return `
+        <div class="arrow-down left">
+            <img src="../../../src/images/arrow.svg" alt="Seta apontada para baixo">
+        </div>
+    `;
+};
+
+export default ArrowDown;
--- src/objects/ScoreBoard/index.js
+++ src/objects/ScoreBoard/index.js
@@ -2,10 +2,12 @@ import "./style.css";
 import PlayerName from "../../components/PlayerName";
 import PlayerScore from "../../components/PlayerScore";
 import VsPlayer from "../../components/VsPlayer";
+import ArrowDown from "../../components/ArrowDown";
 
 const ScoreBoard = () => {
   return `
     <header class="score-board">
+        ${ArrowDown()}
         ${PlayerName("Player1")}
         ${PlayerScore(3)}
         ${VsPlayer()}
