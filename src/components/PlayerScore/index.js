b61ebc70539fc3a58189127909f02923e25cbd40
Adriano <adrianosenny@gmail.com>
2023-04-19 10:50:04 -0300

    :sparkles: Add CSS the of PlayerScore component

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -3,9 +3,9 @@ import "./style.css";
 const PlayerScore = (points = 0) => {
   return `
         <ul class="player-score" data-points="${points}">
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
+            <li class="pointer">1</li>
+            <li class="pointer">2</li>
+            <li class="pointer">3</li>
         </ul>
     `;
 };
